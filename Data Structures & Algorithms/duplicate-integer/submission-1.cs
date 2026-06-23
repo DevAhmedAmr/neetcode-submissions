9c77077aa6d19bd06bb158f675a0bc1ad9f7304f
Ahmed Amr <113526591+DevAhmedAmr@users.noreply.github.com>
2026-06-23 20:18:42 +0300

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public bool hasDuplicate(int[] nums)
+    {
+        
+        var NumsSet = new HashSet<int>();
+
+        foreach (int i in nums)
+        {
+            if (NumsSet.Contains(i)) {
+                return true;
+
+            }
+            
+            NumsSet.Add(i); 
+
+
+            
+                
+          
+
+
+        }
+        return false;
+    }
+}
