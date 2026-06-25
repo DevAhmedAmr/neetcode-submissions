5b0a92719762ede39d332f11a9616cc9647a503f
Ahmed Amr <113526591+DevAhmedAmr@users.noreply.github.com>
2026-06-25 05:19:13 +0300

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+
+        if (s is null || t is null ||  s.Length != t.Length)
+            return false;
+
+        char[] charsCount = new char[255];
+  
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            ++charsCount[s[i]];
+            --charsCount[t[i]];
+
+        }
+
+        foreach (var i in charsCount)
+            if (i != 0)
+                return false;
+
+        return true;
+
+    }
+}
