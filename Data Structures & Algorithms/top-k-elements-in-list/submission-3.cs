695d98370ae8880b35fa9435c8bbdbb4ff856011
Ahmed Amr <113526591+DevAhmedAmr@users.noreply.github.com>
2026-07-02 08:02:10 +0300

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cs
@@ -0,0 +1,81 @@
+    public class Solution
+    {
+//        Input: nums = [1, 2, 2, 3, 3, 3], k = 2
+
+//Output: [2, 3]
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int,int> elementsFrequents = [];
+            int maxDuplication = 0;
+
+            int[] TopKFrequentNumbers = new int[k ];
+
+
+            foreach (var i in nums)
+            {
+                if (!elementsFrequents.ContainsKey(i))
+                    elementsFrequents[i] = 0;
+
+                elementsFrequents[i] += 1;
+
+                if (elementsFrequents[i] > maxDuplication)
+                    maxDuplication = elementsFrequents[i];
+            }
+
+            List<int>[] freq = new List<int>[maxDuplication+1];
+
+            foreach (var pair in elementsFrequents)
+
+            {
+                int number= pair.Key;
+                int duplicationNumber= pair.Value;
+
+
+                if (freq[duplicationNumber] is null)
+                    freq[duplicationNumber] = new List<int> ();
+
+                freq[duplicationNumber].Add(number);
+
+
+
+            }
+
+            int ItemFoundCounter = 0;
+            int j = 0;
+
+            //Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(arr));
+
+            for (int i = freq.Length-1; i>0; i--)
+     
+            {
+                if (freq[i] is null)
+                    continue;
+                foreach (var item  in freq[i])
+                {
+                   // if (item = 0)
+                    TopKFrequentNumbers[j]=item;
+                    //Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(TopKFrequentNumbers));
+
+                    ItemFoundCounter++;
+                    j++;
+
+
+                    if (ItemFoundCounter >= k)
+                        return TopKFrequentNumbers;
+                 
+                }
+
+
+
+
+            }
+            return TopKFrequentNumbers;
+
+
+
+
+
+
+
+
+        }}
