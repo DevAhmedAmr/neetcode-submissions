238d7a1de660d83327cc7081f16987fe2d117405
Ahmed Amr <113526591+DevAhmedAmr@users.noreply.github.com>
2026-07-12 12:36:20 +0300

    Add: buy-and-sell-crypto - submission-12

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-12.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int l=0 , r=0, maxProfit=0;
+
+        for (; r < prices.Length; r++){
+            Console.WriteLine($"l:{l} r:{r} -> {Math.Max(prices[r]-prices[l],maxProfit)}= Math.Max({prices[r]}-{prices[l]},{maxProfit});");
+            if (prices[r]<prices[l])
+            l=r;
+            maxProfit= Math.Max(prices[r]-prices[l],maxProfit);
+
+
+        }
+        return maxProfit;
+    } 
+}
