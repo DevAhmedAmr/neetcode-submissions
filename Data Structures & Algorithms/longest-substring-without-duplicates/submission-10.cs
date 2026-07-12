6fc77636f1511545fb750324066ea8e7abcf9adb
Ahmed Amr <113526591+DevAhmedAmr@users.noreply.github.com>
2026-07-12 14:30:26 +0300

    Add: longest-substring-without-duplicates - submission-10

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-10.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        var set = new HashSet<char>();
+
+        int r = 0, l =0,max=0;
+
+        for (;r<s.Length;r++){
+
+            if (!set.Contains(s[r])){
+                set.Add(s[r]);
+                //s[l..r].Length
+                max = Math.Max(max,r-l+1);
+            }
+            else {
+                while (set.Contains(s[r])){
+                set.Remove(s[l]);
+                l++;
+                }
+                set.Add(s[r]);
+
+            }
+            
+            Console.WriteLine($" {l} {r} | {s[l..r]} | {max} ");
+
+        }
+        return max;
+    }
+}
