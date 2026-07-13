857a57c79e840866ed58c6f918bcbec40c41436b
Ahmed Amr <113526591+DevAhmedAmr@users.noreply.github.com>
2026-07-13 15:45:03 +0300

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        //
+        var charsNum= new Dictionary<char,int>();
+        int l=0,r=0,max=0;
+         int maxFreqLetter=0;
+
+        for (;r<s.Length;r++){
+
+            int charDuplicationNumber=charsNum.GetValueOrDefault(s[r],0);
+            charsNum[s[r]]= charDuplicationNumber+1;
+
+            int windowSize=r-l+1;
+            maxFreqLetter= Math.Max(maxFreqLetter,charsNum[s[r]]);
+
+            
+              while((r-l+1)-maxFreqLetter >k && l<r){
+                charsNum[s[l]]-=1;
+                l++;
+
+              }
+            
+            max=Math.Max(max,r-l+1);
+
+
+        }
+        return max;
+        
+    }
+}
